55fbb075e9a6d3901c05175318825243398cbb3e
Xixi Liang <lxx260905@gmail.com>
2024-04-25 15:28:07 -0700

    test node

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
